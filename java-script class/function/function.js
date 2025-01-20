18fc77419eff374103d1d12805ffb9efdf30d5ef
Maria-Sultana-Meem <sultanamari639@gmail.com>
2025-01-21 02:20:46 +0600

    first function

--- java-script class/function/function.js
+++ java-script class/function/function.js
@@ -3,18 +3,31 @@ function mySkill (){
 document.write('HTML </br>') ;
 document.write('CSS </br>') ;
 document.write('Tailwind </br>') ;
+document.write('Hey </br>') ;
 document.write('..........End........ </br>') ;
 
 }
 
+mySkill();
+mySkill();
+mySkill();
+mySkill();
+
+
+
+function calculation (){
+    let num1=10;
+    let num2=5;
+    document.write(`Total sum=${num1+num2} </br>`)
+    document.write(`Total sum=${num1-num2}</br>`)
+    document.write(`Total sum=${num1*num2} </br>`)
+    document.write(`Total sum=${num1/num2} </br>`)
+
+}
+calculation()
 
 
 
 
 
-/*
-document.write('HTML </br>') ;
-document.write('CSS </br>') ;
-document.write('Tailwind </br>') ;
-document.write('..........End........ </br>') ;
-*/
+
