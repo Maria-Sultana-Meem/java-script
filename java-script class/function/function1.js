d6e7d89785260ffe66f6e53eee40308a50802b44
Maria-Sultana-Meem <sultanamari639@gmail.com>
2025-01-21 11:32:24 +0600

    unary and ternary added

---
+++ java-script class/function/function1.js
@@ -0,0 +1,50 @@
+
+        // ........unary operator.....
+        /*
+        let number1 = parseInt( prompt());
+        let number2 = parseInt(prompt());
+        let number3 = parseInt(prompt());
+
+        let result = number1 + number2 + number3;
+         document.write(result)*/
+       /*
+        let number1 = prompt();
+        let number2 = prompt();
+        let number3 = prompt();
+
+        let result =parseInt(number1) +parseInt (number2) +parseInt (number3);
+         document.write(result) */  
+        /*
+        let number1 = prompt();
+        let number2 = prompt();
+        let number3 = prompt();
+        let result = +number1 + +number2 + +number3
+        document.write(result) */
+         /* 
+        let num = +prompt('Enter your first number :')
+        let num1 = +prompt('Enter your second number :')
+        let result = num + num1
+        document.write(result) */
+
+
+
+//    ......ternary operator.....
+
+/*
+let myAge = 20;
+if(myAge>=18){
+        console.log('you can vote')
+}
+else{
+        console.log('You are not eligible')
+}
+ */       
+//     3condition of ternary operator
+// condition
+// ?
+// :
+
+// condition ? true value (if) : false value (else)
+       let myAge = 17
+myAge>=18 ? console.log('you can vote') : console.log('You are not eligible');
+
